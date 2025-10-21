06d45f83c5316e374bada505dbcde4d42cf9ef86
vatsalawasthi <135455420+vatsalawasthi@users.noreply.github.com>
2025-10-21 18:57:22 +0530

    Time: 3 ms (78.59%) | Memory: 45.6 MB (71.96%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -9,17 +9,13 @@ class Solution {
                 i++;
             }
         }
-    //    List<Integer> ans = new ArrayList<>();
 
         for(int index=0;index<nums.length;index++){
             if(nums[index]!=index+1){
                 return  new int[]{nums[index], index+1};
-        //  ans.add(nums[index]); 
-        //         ans.add(index + 1);   
             }
         }
         return new int[]{-1,-1};
-         //{ans.get(0), ans.get(1)};
     }
 
     void swap(int[] nums, int first, int second) {
