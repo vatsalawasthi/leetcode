5d394da1e1a9d19493b34aaec8ecd6269da9168e
vatsalawasthi <135455420+vatsalawasthi@users.noreply.github.com>
2025-10-25 21:55:47 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (32.10%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -3,12 +3,10 @@ class Solution {
         int length = 0;
         int i = s.length() - 1;
 
-        // Step 1: skip trailing spaces
         while (i >= 0 && s.charAt(i) == ' ') {
             i--;
         }
 
-        // Step 2: count characters until next space or start of string
         while (i >= 0 && s.charAt(i) != ' ') {
             length++;
             i--;
