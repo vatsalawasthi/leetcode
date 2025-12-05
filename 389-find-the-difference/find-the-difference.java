9dbbbe408dfe0d83e8d420f3058cfce8924feefa
vatsalawasthi <135455420+vatsalawasthi@users.noreply.github.com>
2025-12-05 21:05:46 +0530

    Time: 3 ms (40.45%) | Memory: 43.3 MB (8.10%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+
+        // Convert to char arrays and sort them
+        char[] a = s.toCharArray();
+        char[] b = t.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        int i = 0, j = 0;
+
+        // Compare sorted characters
+        while (i < n && j < m) {
+            if (a[i] == b[j]) {
+                i++;
+                j++;
+            } else {
+                return b[j];
+            }
+        }
+
+        return b[m - 1];
+    }
+}
