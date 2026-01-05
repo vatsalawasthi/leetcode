ccff6cb8f611caaf274199c1a6e729c5baf87441
vatsalawasthi <135455420+vatsalawasthi@users.noreply.github.com>
2026-01-05 15:50:56 +0530

    Time: 1 ms (99.69%) | Memory: 42.9 MB (96.04%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,26 +1,23 @@
 class Solution {
     public boolean isValid(String s) {
         char[] stack = new char[s.length()];
-        int top = -1; // stack is empty
+        int top = -1;
 
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
 
-            // push opening brackets
             if (c == '(' || c == '{' || c == '[') {
                 stack[++top] = c;
             } else {
-                // closing bracket: check if stack empty
                 if (top == -1) return false;
 
-                char open = stack[top--]; // pop
+                char open = stack[top--];
                 if (c == ')' && open != '(') return false;
                 if (c == '}' && open != '{') return false;
                 if (c == ']' && open != '[') return false;
             }
         }
 
-        // if stack empty at end → valid
         return top == -1;
     }
 }
