9ef9fab9ecf5a3e92e408dfe44d9809debac9b1e
vatsalawasthi <135455420+vatsalawasthi@users.noreply.github.com>
2026-02-19 23:30:05 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (59.79%) - LeetSync

--- 179-largest-number/largest-number.java
+++ 179-largest-number/largest-number.java
@@ -1,24 +1,25 @@
+import java.util.*;
+
 class Solution {
     public String largestNumber(int[] nums) {
-        Integer[] arr = new Integer[nums.length];
+        String[] arr = new String[nums.length];
         for (int i = 0; i < nums.length; i++) {
-            arr[i] = nums[i];
+            arr[i] = String.valueOf(nums[i]);
         }
-        Arrays.sort(arr, (a, b) -> {
-            String ab = a + "" + b;
-            String ba = b + "" + a;
-            return ba.compareTo(ab);
-        });
-        if (arr[0] == 0)
+
+        Arrays.sort(arr, (a, b) -> (b + a).compareTo(a + b));
+
+        if (arr[0].equals("0")) {
             return "0";
-        StringBuilder sb = new StringBuilder();
-        for (int num : arr) {
-            sb.append(num);
         }
 
-        return sb.toString();
-    }
+        StringBuilder result = new StringBuilder();
+        for (String s : arr) {
+            result.append(s);
+        }
 
+        return result.toString();
+    }
     static {
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             try (java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")) {
