82d3fc3218d988f8ec221362e3e1dbae775e14ad
vatsalawasthi <135455420+vatsalawasthi@users.noreply.github.com>
2026-03-02 12:55:59 +0530

    Time: 3 ms (22.64%) | Memory: 49.5 MB (86.79%) - LeetSync

--- 1658-minimum-swaps-to-arrange-a-binary-grid/minimum-swaps-to-arrange-a-binary-grid.java
+++ 1658-minimum-swaps-to-arrange-a-binary-grid/minimum-swaps-to-arrange-a-binary-grid.java
@@ -29,8 +29,12 @@ class Solution {
     private int TrailingZeros(int[] row) {
         int count = 0;
         for (int i = row.length - 1; i >= 0; i--) {
-            if (row[i] == 0) count++;
-            else break;
+            if (row[i] == 0){
+                count++;
+            }
+            else{
+                break;
+            }
         }
         return count;
     }
