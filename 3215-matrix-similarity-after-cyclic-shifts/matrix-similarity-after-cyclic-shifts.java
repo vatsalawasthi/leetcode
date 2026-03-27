95fb1a56186c088ca829ef038c1100ad9009f222
Vatsal Awasthi <135455420+vatsalawasthi@users.noreply.github.com>
2026-03-27 23:45:30 +0530

    Time: 1 ms (99.47%) | Memory: 47 MB (64.00%) - LeetSync

--- 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.java
+++ 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.java
@@ -2,20 +2,18 @@ class Solution {
     public boolean areSimilar(int[][] mat, int k) {
         int m = mat.length;
         int n = mat[0].length;
-        k = k % n;
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-
+        k = k%n;
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j<n; j++){
                 int expected;
 
-                if (i % 2 == 0) {
-                    expected = mat[i][(j + k) % n];
-                } else {
-                    expected = mat[i][(j - k + n) % n];
+                if(i%2==0){
+                    expected = mat[i][(j+k)%n];
+                }else{
+                    expected = mat[i][(j-k+n)%n];
                 }
-
-                if (mat[i][j] != expected) {
+                if(expected != mat[i][j]){
                     return false;
                 }
             }
