ba6c826736a5512946335f5b0bcad7b62d15734c
Vatsal Awasthi <135455420+vatsalawasthi@users.noreply.github.com>
2026-03-30 19:10:33 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (31.63%) - LeetSync

---
+++ 2102-find-the-middle-index-in-array/find-the-middle-index-in-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findMiddleIndex(int[] nums) {
+        int sum = 0;
+
+        for(int n : nums){
+            sum += n;
+        }
+        int sumbeforePivot = 0;
+
+        for(int i = 0; i<nums.length; i++){
+            int sumafterPivot = sum - nums[i] - sumbeforePivot;
+            if(sumbeforePivot == sumafterPivot){
+                return i;
+            }
+            sumbeforePivot += nums[i];
+        }
+        return -1;
+    }
+}
