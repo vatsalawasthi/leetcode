d8af63fcc5d2a7161fa459ccd46d69dd791c4719
Vatsal Awasthi <135455420+vatsalawasthi@users.noreply.github.com>
2026-05-09 19:54:29 +0530

    Time: 6 ms (44.78%) | Memory: 47.8 MB (11.94%) - LeetSync

---
+++ 2043-cyclically-rotating-a-grid/cyclically-rotating-a-grid.java
@@ -0,0 +1,72 @@
+class Solution {
+    public int[][] rotateGrid(int[][] grid, int k) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int layers = Math.min(m, n) / 2;
+
+        for (int layer = 0; layer < layers; layer++) {
+
+            ArrayList<Integer> list = new ArrayList<>();
+
+            int top = layer;
+            int left = layer;
+            int bottom = m - layer - 1;
+            int right = n - layer - 1;
+
+            // top row
+            for (int j = left; j <= right; j++) {
+                list.add(grid[top][j]);
+            }
+
+            // right column
+            for (int i = top + 1; i <= bottom - 1; i++) {
+                list.add(grid[i][right]);
+            }
+
+            // bottom row
+            for (int j = right; j >= left; j--) {
+                list.add(grid[bottom][j]);
+            }
+
+            // left column
+            for (int i = bottom - 1; i >= top + 1; i--) {
+                list.add(grid[i][left]);
+            }
+
+            int size = list.size();
+            int rot = k % size;
+
+            ArrayList<Integer> rotated = new ArrayList<>();
+
+            // counter-clockwise rotation
+            for (int i = 0; i < size; i++) {
+                rotated.add(list.get((i + rot) % size));
+            }
+
+            int idx = 0;
+
+            // top row
+            for (int j = left; j <= right; j++) {
+                grid[top][j] = rotated.get(idx++);
+            }
+
+            // right column
+            for (int i = top + 1; i <= bottom - 1; i++) {
+                grid[i][right] = rotated.get(idx++);
+            }
+
+            // bottom row
+            for (int j = right; j >= left; j--) {
+                grid[bottom][j] = rotated.get(idx++);
+            }
+
+            // left column
+            for (int i = bottom - 1; i >= top + 1; i--) {
+                grid[i][left] = rotated.get(idx++);
+            }
+        }
+
+        return grid;
+    }
+}
