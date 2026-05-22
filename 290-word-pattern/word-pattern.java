75265888f36b2c9ee5d42238c54d4d8379d65745
Vatsal Awasthi <135455420+vatsalawasthi@users.noreply.github.com>
2026-05-22 22:17:20 +0530

    Time: 1 ms (82.55%) | Memory: 42.5 MB (93.60%) - LeetSync

---
+++ 290-word-pattern/word-pattern.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" ");
+        
+        // Length mismatch
+        if (pattern.length() != words.length) {
+            return false;
+        }
+
+        HashMap<Character, String> charToWord = new HashMap<>();
+        HashMap<String, Character> wordToChar = new HashMap<>();
+
+        for (int i = 0; i < pattern.length(); i++) {
+            char ch = pattern.charAt(i);
+            String word = words[i];
+
+            // Check character -> word mapping
+            if (charToWord.containsKey(ch)) {
+                if (!charToWord.get(ch).equals(word)) {
+                    return false;
+                }
+            } else {
+                charToWord.put(ch, word);
+            }
+
+            // Check word -> character mapping
+            if (wordToChar.containsKey(word)) {
+                if (wordToChar.get(word) != ch) {
+                    return false;
+                }
+            } else {
+                wordToChar.put(word, ch);
+            }
+        }
+
+        return true;
+    }
+}
