8d0c38c316f430a02d5093ada68f9de8445e532e
Vatsal Awasthi <135455420+vatsalawasthi@users.noreply.github.com>
2026-06-23 22:07:31 +0530

    Time: 24 ms (47.54%) | Memory: 48.3 MB (42.73%) - LeetSync

---
+++ 227-basic-calculator-ii/basic-calculator-ii.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int calculate(String s) {
+        Stack<Integer> stack = new Stack<>();
+        
+        int num = 0;
+        char op = '+';
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (Character.isDigit(ch)) {
+                num = num * 10 + (ch - '0');
+            }
+            if ((!Character.isDigit(ch) && ch != ' ') || i == s.length() - 1){
+                if (op == '+') {
+                    stack.push(num);
+                } 
+                else if (op == '-') {
+                    stack.push(-num);
+                } 
+                else if (op == '*') {
+                    stack.push(stack.pop() * num);
+                } 
+                else if (op == '/') {
+                    stack.push(stack.pop() / num);
+                }
+                op = ch;
+                num = 0;
+            } 
+        }
+        int result = 0;
+        while (!stack.isEmpty()) {
+            result += stack.pop();
+        }
+        return result;
+    }
+}
