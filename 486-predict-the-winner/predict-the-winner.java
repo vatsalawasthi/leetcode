fca718952b5f71638f05d3addd0aff80b8febfc6
Vatsal Awasthi <135455420+vatsalawasthi@users.noreply.github.com>
2026-07-15 22:03:59 +0530

    Time: 1 ms (49.77%) | Memory: 42.8 MB (40.28%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean predictTheWinner(int[] nums) {
+        int n = nums.length;
+        int[][] dp = new int[n][n];
+
+        for(int i = 0; i<n; i++){
+            dp[i][i] = nums[i];
+        }
+
+        for(int len = 2; len<=n; len++){
+            for(int i = 0; i<=n-len; i++){
+                int j = i+len-1;
+
+                int pickLeft = nums[i] - dp[i + 1][j];
+                int pickRight = nums[j] - dp[i][j - 1];
+
+                dp[i][j] = Math.max(pickLeft, pickRight);
+            }
+        }
+        if(dp[0][n-1]>=0){
+            return true;
+        }
+        return false;
+    }
+}
